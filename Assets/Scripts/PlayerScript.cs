5486539a70a5985eecd25ab5ab4eae77cc252cb1
WormClay <kulya81@rambler.ru>
2022-08-26 21:59:27 +0600

    Lesson2
    Lesson2

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] CharacterSettings settings;
+    [SerializeField] [Range(0, 100)] private int myHelth;
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 100, 200, 20), $"Helth: {myHelth}");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myHelth = settings.maxHelth;
+    }
+
+}
