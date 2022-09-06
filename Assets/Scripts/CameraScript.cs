e2f47d28dd0dfdc399858e8aa0910eee6dc39a3e
WormClay <kulya81@rambler.ru>
2022-09-06 21:45:01 +0600

    Lesson4
    Lesson4

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    Vector3 rot = new Vector3(0, 0, 0);
-    [SerializeField] private float mouseSpeed = 1f; // Чувствительность мыши
+    //Vector3 rot = new Vector3(0, 0, 0);
+    //[SerializeField] private float mouseSpeed = 1f; // Чувствительность мыши
+    [SerializeField] private Transform hero;
+    public float distanceAway = 1.7f;
+    public float distanceUp = 1.3f;
+    public float smooth = 2f;
+
+    private Vector3 TargetPosition;
 
     void Update()
     {
-        float MouseX = Input.GetAxis("Mouse X") * mouseSpeed;
+        /*float MouseX = Input.GetAxis("Mouse X") * mouseSpeed;
         float MouseY = Input.GetAxis("Mouse Y") * mouseSpeed;
         rot.x = rot.x - MouseY;
         rot.y = rot.y + MouseX;
         rot.z = 0;
         //transform.eulerAngles = new Vector3(0, rot.y, 0);
-        transform.eulerAngles = rot;
+        transform.eulerAngles = rot;*/
+
+
+        TargetPosition = hero.position + Vector3.up * distanceUp - hero.forward * distanceAway;
+        transform.position = Vector3.Lerp(transform.position, TargetPosition, Time.deltaTime * smooth);
+        transform.LookAt(hero);
     }
 }
